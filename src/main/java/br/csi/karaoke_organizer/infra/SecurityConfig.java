680420f643f3b0b3bb3b488b61df7c580cf5b75a
JuJaeger06 <juliacjaeger06@gmail.com>
2025-12-01 02:27:42 -0300

    Ajustado Back

--- src/main/java/br/csi/karaoke_organizer/infra/SecurityConfig.java
+++ src/main/java/br/csi/karaoke_organizer/infra/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,8 +35,10 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(crsf -> crsf.disable())
+                .cors(Customizer.withDefaults())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() // **NOVO: Libera o pre-flight**
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/funcionarios").permitAll()
                         .requestMatchers("/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
---
+++ src/main/resources/db/migration/V5__insert-table-mesas.sql
@@ -0,0 +1,6 @@
+INSERT INTO mesas (nome_mesa) VALUES
+    ('Mesa 1'),
+    ('Mesa 2'),
+    ('Mesa 3'),
+    ('Mesa 4'),
+    ('Mesa 5');
